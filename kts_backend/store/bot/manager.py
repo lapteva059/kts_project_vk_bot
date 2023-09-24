d707b69dc7bb1b4d798f8f3b23bf4403d7b0f4af
Irina <lapteva059@gmail.com>
2023-09-25 02:39:02 +0300

    create vk server, echo bot

---
+++ kts_backend/store/bot/manager.py
@@ -0,0 +1,21 @@
+import typing
+
+from kts_backend.store.vk_api.dataclasses import Update, Message
+
+if typing.TYPE_CHECKING:
+    from kts_backend.web.app import Application
+
+
+class BotManager:
+    def __init__(self, app: "Application"):
+        self.app = app
+        self.bot = None
+
+    async def handle_updates(self, updates: list[Update]):
+        for update in updates:
+            await self.app.store.vk_api.send_message(
+                Message(
+                    user_id=update.object.message.id,
+                    text=update.object.message.text
+                )
+            )
