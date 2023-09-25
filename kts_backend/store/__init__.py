5be255547be6ad441bfc32c2500a79136d3aad16
Irina <lapteva059@gmail.com>
2023-09-25 03:43:59 +0300

    add accessor.py

--- kts_backend/store/__init__.py
+++ kts_backend/store/__init__.py
@@ -1,5 +1,13 @@
 class Store:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, app: "Application"):
         from kts_backend.users.accessor import UserAccessor
+        from kts_backend.store.bot.manager import BotManager
+        from kts_backend.store.vk_api.accessor import VkApiAccessor
 
-        self.user = UserAccessor(self)
+        self.user = UserAccessor()
+        self.bots_manager = BotManager(app)
+        self.vk_api = VkApiAccessor(app)
+
+
+def setup_store(app: "Application"):
+    app.store = Store(app)
--- kts_backend/web/app.py
+++ kts_backend/web/app.py
@@ -10,12 +10,18 @@ from pyparsing import Optional
 
 from kts_backend import __appname__, __version__
 from .urls import register_urls
-
-
-__all__ = ("ApiApplication",)
+from kts_backend.store import setup_store, Store
 
 
 class Application(AiohttpApplication):
     config = None
     store = None
     database = None
+
+app = Application()
+
+
+def setup_app(config_path: str) -> Application:
+    setup_config(app, config_path)
+    setup_store(app)
+    return app
