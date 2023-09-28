1368b1057731cb0436ec1b3b6772345719209125
Irina <lapteva059@gmail.com>
2023-09-28 13:20:05 +0300

    fix

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
             config_path=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)), "etc/config.yaml"
             )
-        )
+        ), host='127.0.0.1', port='8000'
     )
